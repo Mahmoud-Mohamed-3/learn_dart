c1f1548d4e0f6c27caaa3c87d48bb3617c376ae9
Mahmoud-Mohamed-3 <mahmoudfalous@gmail.com>
2025-04-21 01:34:33 +0200

    First Day Syntax and Data types

---
+++ lib/variable.dart
@@ -0,0 +1,20 @@
+void main(){
+  // type of variable => Name of variable => value
+  // The Same as any programming language And don't forget to use the semicolon at the end of each line
+
+  // $ is used before the variable name when the variable is a string
+  String name = 'MaNo';
+  // The correct way to print a variable is to use the $ sign before the variable name if the variable is a string or the var without the $ sign if it isn't a string
+  print(name);
+  print ("$name");
+  // That isn't a Variable just a string 👇
+  print ("name");
+  // don't use the reserve words as a variable name like => and, class, void, int, double, String, var, final, const, static, dynamic
+
+  // Integers
+  int age = 19 ;
+  print (age);
+  // That will overwrite the value of the variable
+  age = 20 ;
+  print (age);
+}
